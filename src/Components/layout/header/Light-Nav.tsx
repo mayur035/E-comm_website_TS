7dd6702087d3ad5970479a4f381937ad97bafbcd
mayur035 <mgp312002@gmail.com>
2024-03-03 20:27:04 +0530

    some header changes

--- src/Components/layout/header/Light-Nav.tsx
+++ src/Components/layout/header/Light-Nav.tsx
@@ -7,27 +7,36 @@ import { Link } from 'react-router-dom'
 
 const LightNavbar = () => {
     const [Mobile, setMobile] = useState<boolean>(false)
+
+    const toggleMobileMenu = () => {
+        setMobile(!Mobile);
+    }
+
+    const closeMobileMenu = () => {
+        setMobile(false);
+    }
+
     return (
         <section id={classes['light-header']}>
             <div className={classes.container}>
-                <Logo/>
+                <Logo />
                 <div className={`${classes.menu} ${Mobile ? classes['menu-res'] : ""}`}>
                     <ul>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/productListing'>Shop</Link></li>
-                        <li><Link to='/aboutUs'>About</Link></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><Link to='/contactUs'>Contact</Link></li>
-                        <li><a href="#">Pages</a></li>
+                        <li><Link to='/' onClick={closeMobileMenu}>Home</Link></li>
+                        <li><Link to='/productListing' onClick={closeMobileMenu}>Shop</Link></li>
+                        <li><Link to='/aboutUs' onClick={closeMobileMenu}>About</Link></li>
+                        <li><a href="#" onClick={closeMobileMenu}>Blog</a></li>
+                        <li><Link to='/contactUs' onClick={closeMobileMenu}>Contact</Link></li>
+                        <li><a href="#" onClick={closeMobileMenu}>Pages</a></li>
                     </ul>
                     <ul>
                         <li><a href="#"><PersonOutlineOutlined /> Login/Signup</a></li>
                         <li><a href="#"><SearchOutlined /></a></li>
-                        <li><Link to='/cart'><ShoppingCartOutlined /></Link></li>
-                        <li><a href="#"><FavoriteBorderOutlined /></a></li>
+                        <li><Link to='/cart' onClick={closeMobileMenu}><ShoppingCartOutlined /></Link></li>
+                        <li><a href="#" onClick={closeMobileMenu}><FavoriteBorderOutlined /></a></li>
                     </ul>
                 </div>
-                <button onClick={() => { setMobile(!Mobile) }} className={classes['res-Nav']}>
+                <button onClick={toggleMobileMenu} className={classes['res-Nav']}>
                     {Mobile ? <Close /> : <Menu />}
                 </button>
             </div>
